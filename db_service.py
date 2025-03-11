6e20ba2dfb99e540eea60d1fecab520c6828d253
Apapron Pipatpanuwittaya <meanee3003@gmail.com>
2025-03-12 06:22:38 +0700

    Update database connection to MySQL

--- db_service.py
+++ db_service.py
@@ -3,14 +3,31 @@ import json
 import os
 
 def create_connection():
+    # ดึงค่าจาก environment variable
+    host = os.getenv("MYSQLHOST")
+    if not host:
+        host = "ballast.proxy.rlwy.net"
+    
+    user = os.getenv("MYSQLUSER")
+    if not user:
+        user = "root"
+    
+    password = os.getenv("MYSQLPASSWORD")
+    if not password:
+        password = "FrmfJcaQNVQXdOsAtBuMQopdrwrqmPdF"
+    
+    database = os.getenv("MYSQLDATABASE")
+    if not database:
+        database = "railway"
+    
     port_env = os.getenv("MYSQLPORT")
     port = int(port_env) if port_env and port_env.isdigit() else 55288
 
     return mysql.connector.connect(
-        host=os.getenv("MYSQLHOST", "ballast.proxy.rlwy.net"),
-        user=os.getenv("MYSQLUSER", "root"),
-        password=os.getenv("MYSQLPASSWORD", "FrmfJcaQNVQXdOsAtBuMQopdrwrqmPdF"),
-        database=os.getenv("MYSQLDATABASE", "railway"),
+        host=host,
+        user=user,
+        password=password,
+        database=database,
         port=port
     )
 
