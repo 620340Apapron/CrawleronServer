a14db80b980177c36cd9bcb9e480d567b44ed506
Apapron Pipatpanuwittaya <meanee3003@gmail.com>
2025-08-06 07:46:40 +0700

    update cralwer

--- crawlers/niin.py
+++ crawlers/niin.py
@@ -58,7 +58,7 @@ def scrape_naiin_detail_page(driver, book_url):
 
 def get_all_book_urls(driver, max_pages=999):
     urls = set()
-    base_url = "https://www.naiin.com/search?q=book&page="
+    base_url = "https://www.naiin.com/category?category_1_code=13&product_type_id=1"
     
     for p in range(1, max_pages + 1):
         print(f"[*] [naiin] กำลังรวบรวม URL จากหน้า {p}...")
