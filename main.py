94eb21cf6abfca660140290ec914dba8594f5dd6
Apapron Pipatpanuwittaya <meanee3003@gmail.com>
2026-05-08 10:37:19 +0700

    Update Crawler

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import os
 import shutil
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.chrome.service import Servic
+from selenium.webdriver.chrome.service import Service
 import json
 from decimal import Decimal
 from datetime import date, datetime
